019b02034b3fc480a754c6bceef47b630a91b2b9
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-08-23 00:18:06 +0800

    feat: mmd-loader (#1)
    * feat: mmd-loader
    
    * chore(eslint): ts config
    
    * refactor: keep original code

--- eslint.config.ts
+++ eslint.config.ts
@@ -3,4 +3,8 @@ import { defineConfig } from '@moeru/eslint-config'
 export default defineConfig({
   pnpm: true,
   react: true,
+}, {
+  rules: {
+    'sonarjs/cognitive-complexity': 'off',
+  },
 })
---
+++ packages/three-mmd/src/loaders/MMDLoader.d.ts
@@ -0,0 +1,60 @@
+import type { AnimationClip, Camera, FileLoader, Loader, LoadingManager, SkinnedMesh } from 'three'
+
+export interface MMDLoaderAnimationObject {
+  animation: AnimationClip
+  mesh: SkinnedMesh
+}
+
+/**
+ * @deprecated The module has been deprecated and will be removed with r172. Please migrate to
+ * https://github.com/takahirox/three-mmd-loader instead.
+ */
+export class MMDLoader extends Loader<SkinnedMesh> {
+  animationBuilder: object
+  animationPath: string
+  loader: FileLoader
+  meshBuilder: object
+  parser: null | object
+  constructor(manager?: LoadingManager)
+
+  loadAnimation(
+    url: string,
+    object: Camera | SkinnedMesh,
+    onLoad: (object: AnimationClip | SkinnedMesh) => void,
+    onProgress?: (event: ProgressEvent) => void,
+    onError?: (event: ErrorEvent) => void,
+  ): void
+  loadPMD(
+    url: string,
+    onLoad: (object: object) => void,
+    onProgress?: (event: ProgressEvent) => void,
+    onError?: (event: ErrorEvent) => void,
+  ): void
+  loadPMX(
+    url: string,
+    onLoad: (object: object) => void,
+    onProgress?: (event: ProgressEvent) => void,
+    onError?: (event: ErrorEvent) => void,
+  ): void
+  loadVMD(
+    url: string,
+    onLoad: (object: object) => void,
+    onProgress?: (event: ProgressEvent) => void,
+    onError?: (event: ErrorEvent) => void,
+  ): void
+  loadVPD(
+    url: string,
+    isUnicode: boolean,
+    onLoad: (object: object) => void,
+    onProgress?: (event: ProgressEvent) => void,
+    onError?: (event: ErrorEvent) => void,
+  ): void
+  loadWithAnimation(
+    url: string,
+    vmdUrl: string | string[],
+    onLoad: (object: MMDLoaderAnimationObject) => void,
+    onProgress?: (event: ProgressEvent) => void,
+    onError?: (event: ErrorEvent) => void,
+  ): void
+  setAnimationPath(animationPath: string): this
+}
