45b83b5448d0e73eacba480ced55620a154e1152
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-10-12 23:04:45 +0800

    chore(three-mmd-b): fix bones

--- packages/three-mmd-b/src/utils/build-bones.ts
+++ packages/three-mmd-b/src/utils/build-bones.ts
@@ -4,25 +4,24 @@ import type { SkinnedMesh } from 'three'
 import { Bone } from 'three'
 
 export const buildBones = (pmx: PmxObject, mesh: SkinnedMesh): Bone[] => {
-  const bones: Bone[] = []
-  pmx.bones.forEach((boneInfo) => {
+  const bones = pmx.bones.map((boneInfo) => {
     const bone = new Bone()
-
     bone.name = boneInfo.name
 
-    const { position } = boneInfo
+    const pos = [...boneInfo.position] as [number, number, number]
     if (boneInfo.parentBoneIndex >= 0 && boneInfo.parentBoneIndex < pmx.bones.length) {
-      position[0] -= pmx.bones[boneInfo.parentBoneIndex].position[0]
-      position[1] -= pmx.bones[boneInfo.parentBoneIndex].position[1]
-      position[2] -= pmx.bones[boneInfo.parentBoneIndex].position[2]
+      const parentInfo = pmx.bones[boneInfo.parentBoneIndex]
+      pos[0] -= parentInfo.position[0]
+      pos[1] -= parentInfo.position[1]
+      pos[2] -= parentInfo.position[2]
     }
-    bone.position.fromArray(position)
-
-    bones.push(bone)
+    bone.position.fromArray(pos)
+    return bone
   })
+
   pmx.bones.forEach((boneInfo, i) => {
     if (boneInfo.parentBoneIndex >= 0 && boneInfo.parentBoneIndex < pmx.bones.length)
-      bones.at(boneInfo.parentBoneIndex)!.add(bones[i])
+      bones[boneInfo.parentBoneIndex].add(bones[i])
     else
       mesh.add(bones[i])
   })
