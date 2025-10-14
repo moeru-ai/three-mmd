a693d3e755566cc231a377ce12363ec967c9f3d4
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-10-14 19:02:35 +0800

    chore(playground): update skeleton demo

--- examples/playground/src/pages/b/skeleton.tsx
+++ examples/playground/src/pages/b/skeleton.tsx
@@ -1,19 +1,30 @@
 import type { SkinnedMesh } from 'three'
 
 import { MMDLoader } from '@moeru/three-mmd'
-import { MMDLoader as BabylonMMDLoader } from '@moeru/three-mmd-b'
+import { MMDLoader as BabylonMMDLoader, buildAnimation, VMDLoader } from '@moeru/three-mmd-b'
+import { useAnimations } from '@react-three/drei'
+import { useLoader } from '@react-three/fiber'
 import { useControls } from 'leva'
 import { startTransition, useEffect, useMemo, useState } from 'react'
 
 import pmdUrl from '../../../../basic/src/assets/miku/miku_v2.pmd?url'
+import vmdUrl from '../../../../basic/src/assets/vmds/wavefile_v2.vmd?url'
 
 const BSkeleton = () => {
-  const { babylonMMD } = useControls({ babylonMMD: true })
+  const {
+    babylonMMD,
+    showSkeleton,
+    withAnimation,
+  } = useControls({
+    babylonMMD: true,
+    showSkeleton: false,
+    withAnimation: false,
+  })
 
   const mmdLoader = useMemo(() => new MMDLoader(), [])
   const babylonMmdLoader = useMemo(() => new BabylonMMDLoader(), [])
 
-  const [object, setObject] = useState<SkinnedMesh>()
+  const [object, setObject] = useState<SkinnedMesh>(useLoader(BabylonMMDLoader, pmdUrl))
   useEffect(() => {
     startTransition(async () => {
       let mesh: SkinnedMesh
@@ -23,18 +34,30 @@ const BSkeleton = () => {
         mesh = await mmdLoader.loadAsync(pmdUrl)
 
       setObject(mesh)
-
-      console.warn(mesh.skeleton.bones[0])
     })
   }, [babylonMMD, babylonMmdLoader, mmdLoader])
 
-  if (!object)
-    return
+  const vmd = useLoader(VMDLoader, vmdUrl)
+  const animation = useMemo(() => {
+    const animation = buildAnimation(vmd, object)
+    animation.name = 'wavefile'
+    return animation
+  }, [vmd, object])
+  const { actions, ref } = useAnimations([animation])
+
+  useEffect(() => {
+    if (withAnimation)
+      actions.wavefile?.play()
+
+    return () => {
+      actions.wavefile?.stop()
+    }
+  }, [actions, withAnimation])
 
   return (
     <>
-      <primitive object={object} scale={0.1} />
-      <skeletonHelper args={[object]} />
+      <primitive object={object} ref={ref} rotation={[0, Math.PI, 0]} scale={0.1} />
+      {showSkeleton && <skeletonHelper args={[object]} />}
     </>
   )
 }
