a340756c399d807d123b9b60ec0029ce9b5a4b31
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-11-18 17:44:20 +0800

    chore(playground): update

--- examples/playground/src/pages/debug/mesh.tsx
+++ examples/playground/src/pages/debug/mesh.tsx
@@ -1,11 +1,25 @@
-import { useLoader } from '@react-three/fiber'
+import type { SkinnedMesh } from 'three'
+
+import { useControls } from 'leva'
+import { startTransition, useEffect, useMemo, useState } from 'react'
 
 import pmdUrl from '../../../../basic/src/assets/miku/miku_v2.pmd?url'
 import { useMMDLoader } from '../../hooks/use-mmd-loader'
 
 const Mesh = () => {
   const MMDLoader = useMMDLoader()
-  const object = useLoader(MMDLoader, pmdUrl)
+  const loader = useMemo(() => new MMDLoader(), [MMDLoader])
+
+  const [object, setObject] = useState<SkinnedMesh>()
+
+  useEffect(() => startTransition(async () => setObject(await loader.loadAsync(pmdUrl))), [loader])
+
+  const { showSkeleton } = useControls({
+    showSkeleton: false,
+  })
+
+  if (object == null)
+    return
 
   return (
     <>
@@ -14,9 +28,11 @@ const Mesh = () => {
         rotation={[0, Math.PI, 0]}
         scale={0.1}
       />
-      <skeletonHelper
-        args={[object]}
-      />
+      {showSkeleton && (
+        <skeletonHelper
+          args={[object]}
+        />
+      )}
     </>
   )
 }
