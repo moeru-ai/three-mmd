4526368b55a50ac8c867800a6c45fe60bdc567ad
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-11-22 21:14:59 +0800

    feat(three-mmd-b): init spring bone manager

--- packages/three-mmd-b/src/utils/mmd.ts
+++ packages/three-mmd-b/src/utils/mmd.ts
@@ -2,6 +2,8 @@ import type { PmxObject } from 'babylon-mmd/esm/Loader/Parser/pmxObject'
 import type { SkinnedMesh } from 'three'
 import type { IK } from 'three/examples/jsm/animation/CCDIKSolver.js'
 
+import { VRMSpringBoneManager } from '@pixiv/three-vrm-springbone'
+
 import type { Grant } from './build-grants'
 
 import { buildGrants } from './build-grants'
@@ -13,9 +15,17 @@ export class MMD {
   public iks: IK[]
   public mesh: SkinnedMesh
 
+  private springBoneManager: VRMSpringBoneManager
+
   constructor(pmx: PmxObject, resourcePath: string) {
     this.grants = buildGrants(pmx)
     this.iks = buildIK(pmx)
     this.mesh = buildMesh(pmx, resourcePath)
+
+    this.springBoneManager = new VRMSpringBoneManager()
+  }
+
+  public update(delta: number) {
+    this.springBoneManager.update(delta)
   }
 }
