d6da35cd3c0b0f26a48bddd35130406858816669
Lilia_Chen <chenalenavenger@gmail.com>
2025-12-05 05:38:26 +0000

    fix(three-mmd): fixed the use functions (#25)
    * fix(three-mmd): fixed the ammo.tsx playground
    
    * chore: fix example
    
    ---------
    
    Co-authored-by: 藍+85CD <50108258+kwaa@users.noreply.github.com>

--- packages/three-mmd-r3f/src/hooks/use-mmd-animation.ts
+++ packages/three-mmd-r3f/src/hooks/use-mmd-animation.ts
@@ -2,19 +2,23 @@ import type { Camera } from '@react-three/fiber'
 import type { AnimationClip, SkinnedMesh } from 'three'
 
 import { buildAnimation, buildCameraAnimation } from '@moeru/three-mmd'
+import { useMemo } from 'react'
 
 import { useVMD } from './use-vmd'
 
 const useMMDAnimation = (vmdPath: string, object: Camera | SkinnedMesh, name?: string): AnimationClip => {
   const vmd = useVMD(vmdPath)
-  const clip = ('isCamera' in object && object.isCamera)
-    ? buildCameraAnimation(vmd)
-    : buildAnimation(vmd, object as SkinnedMesh)
 
-  if (name != null)
-    clip.name = name
+  return useMemo(() => {
+    const c = ('isCamera' in object && object.isCamera)
+      ? buildCameraAnimation(vmd)
+      : buildAnimation(vmd, object as SkinnedMesh)
 
-  return clip
+    if (name != null)
+      c.name = name
+
+    return c
+  }, [vmd, object, name])
 }
 
 // eslint-disable-next-line @masknet/no-top-level
