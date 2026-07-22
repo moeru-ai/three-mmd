cda94a04538b14085fe2539a2aa83b501facf9dc
藍+85CD <50108258+kwaa@users.noreply.github.com>
2026-07-22 22:27:27 +0800

    refactor!: move grant solver, ccdik solver to mmd interface

--- examples/playground/src/pages/index.tsx
+++ examples/playground/src/pages/index.tsx
@@ -1,23 +1,34 @@
-import { useMMDAnimation, useMMDMesh } from '@moeru/three-mmd-r3f'
+import { useMMD, useMMDAnimation } from '@moeru/three-mmd-r3f'
 import { useAnimations } from '@react-three/drei'
+import { useFrame } from '@react-three/fiber'
 import { useEffect } from 'react'
 
 import vmdUrl from '../../../assets/Telephone/モーションデータ(forMMD)/telephone_motion.vmd?url'
 import pmxUrl from '../../../assets/げのげ式初音ミク/げのげ式初音ミク.pmx?url'
 
 const Index = () => {
-  const object = useMMDMesh(pmxUrl)
-  const animation = useMMDAnimation(vmdUrl, object, 'dance')
-  const { actions, ref } = useAnimations([animation])
+  const mmd = useMMD(pmxUrl)
+  const animation = useMMDAnimation(vmdUrl, mmd.mesh, 'dance')
+  const { actions } = useAnimations([animation], mmd.mesh)
+
+  // Keep this after `useAnimations`: both use priority 0, so the generic mixer
+  // samples the VMD first and MMD then applies its IK and grants.
+  useFrame((_, delta) => mmd.update(delta))
 
   useEffect(() => {
-    actions?.dance?.play()
-  })
+    const action = actions?.dance
+    action?.play()
+
+    return () => {
+      action?.stop()
+      mmd.mesh.pose()
+    }
+  }, [actions, mmd])
 
   return (
     <>
-      <primitive object={object} ref={ref} scale={0.1} />
-      <skeletonHelper args={[object]} />
+      <primitive object={mmd.mesh} scale={0.1} />
+      <skeletonHelper args={[mmd.mesh]} />
     </>
   )
 }
