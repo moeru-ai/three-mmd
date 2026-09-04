bb89849f3b8bb0d940fe3e448edfdff69007d3c7
藍+85CD <50108258+kwaa@users.noreply.github.com>
2026-09-04 21:00:13 +0800

    chore(mmd-physical-material): mark as experimental

--- packages/three-mmd/src/materials/physical/mmd-physical-material.ts
+++ packages/three-mmd/src/materials/physical/mmd-physical-material.ts
@@ -68,7 +68,10 @@ const createPhysicalParameters = (
   }
 }
 
-/** Opt-in physically based MMD material backend for WebGLRenderer. */
+/**
+ * @experimental
+ * Opt-in physically based MMD material backend for WebGLRenderer.
+ */
 export class MMDPhysicalMaterial extends MeshPhysicalMaterial {
   public static readonly isMMDMaterial = true as const
   public static readonly mmdCapabilities = capabilities
